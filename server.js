8448c2323e2a9c4d3474531a03c83cf436a09e81
Silas Alvares <83738111+cxryoff@users.noreply.github.com>
2025-06-29 00:28:10 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -9,23 +9,19 @@ app.use(express.static(__dirname));
 
 // Handle routes for your HTML files
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'quiz.html'));
+  res.sendFile(path.join(__dirname, 'verificar.html'));
 });
 
-app.get('/quiz', (req, res) => {
-  res.sendFile(path.join(__dirname, 'quiz.html'));
+app.get('/verificar', (req, res) => {
+  res.sendFile(path.join(__dirname, 'verificar.html'));
 });
 
 app.get('/senha', (req, res) => {
   res.sendFile(path.join(__dirname, 'senha.html'));
 });
 
-app.get('/resultado', (req, res) => {
-  res.sendFile(path.join(__dirname, 'resultado.html'));
-});
-
-app.get('/verificar', (req, res) => {
-  res.sendFile(path.join(__dirname, 'verificar.html'));
+app.get('/final', (req, res) => {
+  res.sendFile(path.join(__dirname, 'final.html'));
 });
 
 app.listen(PORT, () => {
